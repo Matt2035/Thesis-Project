7d92340f5308da235693f6a7a8f0a2f84c277bda
Matt2035 <87042830+Matt2035@users.noreply.github.com>
2023-04-16 22:17:51 -0400

    Update agent.js

--- bot/src/agent.js
+++ bot/src/agent.js
@@ -5,7 +5,6 @@ const {
   ethers,
 } = require("forta-agent");
 
-
 // This bot is not built for scale. Only returns info/warning about the first pinned transaction in the array.
 // If multiple occur in the same block it will only print info on the first one. This is good enough for a simple
 // demonstration with only one transaction at a time
@@ -19,78 +18,93 @@ function hex_to_ascii(str1) {
   return str.trim();
 }
 
+// let proxySC = "0x2784dABad83D25b64880FF514CdA6D287d7D0122;
+let proxySC = "<your-proxySC-address-here>";
+
 async function main(message) {
+  console.log(message);
+  const socketio = require("socket.io-client");
 
-	const socketio = require('socket.io-client');
+  const socket = socketio.connect("http://localhost:3000");
 
-	const socket = socketio.connect('http://localhost:3000');
+  socket.on("connect", () => {
+    console.log("Connected to server");
+    socket.emit("message", proxySC.concat(",", message));
+  });
 
-	socket.on('connect', () => {
-  		console.log('Connected to server');
- 		socket.emit('message', message);
-	});
+  socket.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    socket.disconnect();
+  });
 
-	socket.on('message', (message) => {
-  		console.log(`Received message: ${message}`);
-	});
+  socket.on("disconnect", () => {
+    console.log("Disconnected from server");
+  });
 
-	socket.on('disconnect', () => {
-  		console.log('Disconnected from server');
-	});
-
-  	console.log("Main complete");
+  // console.log("Main complete");
 }
 
 let findingsCache = [];
-let isScanningGoerli = false;
-let currentGoerliBlockNumber = -1;
-// const RINKEBY_RPC_URL =
-//   "https://rinkeby.infura.io/v3/d855c3d2acb54bf5a5a967e04e39c730";
-const Goerli_RPC_URL =
-  "https://goerli.infura.io/v3/d855c3d2acb54bf5a5a967e04e39c730";
-const goerliProvider = new ethers.providers.JsonRpcProvider(Goerli_RPC_URL);
+let isScanningTestnet = false;
+let currentTestnetBlockNumber = -1;
+//const Testnet_RPC_URL =
+//  "https://sepolia.infura.io/v3/d855c3d2acb54bf5a5a967e04e39c730";
+const Testnet_RPC_URL =
+  "https://sepolia.infura.io/v3/<your-infura-api>";
+const TestnetProvider = new ethers.providers.JsonRpcProvider(Testnet_RPC_URL);
 
 let findingsCount = 0;
 
 async function initialize() {
-  currentGoerliBlockNumber = await goerliProvider.getBlockNumber();
+  currentTestnetBlockNumber = await TestnetProvider.getBlockNumber();
 }
 
-async function scanGoerliBlocks() {
-  isScanningGoerli = true;
+async function scanTestnetBlocks() {
+  isScanningTestnet = true;
 
-  const latestGoerliBlockNumber = await goerliProvider.getBlockNumber();
-  while (currentGoerliBlockNumber <= latestGoerliBlockNumber) {
+  const latestTestnetBlockNumber = await TestnetProvider.getBlockNumber();
+  while (currentTestnetBlockNumber <= latestTestnetBlockNumber) {
     // fetch Goerli block
-    const goerliBlock = await goerliProvider.getBlock(currentGoerliBlockNumber);
+    const TestnetBlock = await TestnetProvider.getBlock(
+      currentTestnetBlockNumber
+    );
 
-    console.log("goerli block", currentGoerliBlockNumber);
-    goerliProvider
-      .getLogs({
-        address: "0xE2D8b8A028Fa1bEB7A158d7d2D620df829c5B1c6",
-        fromBlock: currentGoerliBlockNumber,
-        toBlock: currentGoerliBlockNumber,
-        // fromBlock: 8353331,
-        // toBlock: 8353331,
- //        topics: [
-        //   // the 0x508... hexidecimal represents keccak256(TokensLocked(address,bytes32,uint256,uint256))
-        //   // '0x508bc5d33807c46db43ce875d48dca24de9d41f9fcb35ce6e51daed771fafe59',
-        //   // the 0xddf... hexidecimal represents keccak256(Transfer(address,address,uint256)
-        //   //"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    console.log("Testnet block", currentTestnetBlockNumber);
+    TestnetProvider.getLogs({
+      //address: "0xE2D8b8A028Fa1bEB7A158d7d2D620df829c5B1c6",// this is for goerli
+      //address: "0xB3137189275a07eb82E34D679bcb6E0C14F36f62", // address of Message.sol contract
+	address: "<your-Message.sol-contract-address-here",
+      fromBlock: currentTestnetBlockNumber,
+      toBlock: currentTestnetBlockNumber,
+      // fromBlock: 8684074,
+      // toBlock: 8684074,
+      //  topics: [
+      //   // the 0x508... hexidecimal represents keccak256(TokensLocked(address,bytes32,uint256,uint256))
+      //   // '0x508bc5d33807c46db43ce875d48dca24de9d41f9fcb35ce6e51daed771fafe59',
+      //   // the 0xddf... hexidecimal represents keccak256(Transfer(address,address,uint256)
+      //   //"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 
-        //   // THESE TOPICS ARE FOR EVENTS, NOT FUNCTIONS!
+      //   // THESE TOPICS ARE FOR EVENTS, NOT FUNCTIONS!
 
-   //        "0x51a7f65c6325882f237d4aeb43228179cfad48b868511d508e24b4437a819137",
-     //    ],
-      })
-      .then((res) => {
-        // let value = parseInt(res[0]["data"] * 10 ** -18);
-        // console.log(res);
-        if (Object.keys(res).length !== 0) {
+      //        "0x51a7f65c6325882f237d4aeb43228179cfad48b868511d508e24b4437a819137",
+      //    ],
+    }).then((res) => {
+      console.log(Object.keys(res));
+      // let value = parseInt(res[0]["data"] * 10 ** -18);
+      // console.log(res);
+      if (Object.keys(res).length !== 0) {
+        let packet = "";
+        for (let i = 0; i < Object.keys(res).length; i++) {
           console.log("found");
-          dataObject = hex_to_ascii(res[0]["data"]);
+          dataObject = hex_to_ascii(res[i]["data"]);
           console.log(dataObject);
-          main(dataObject);
+          if (i == 0) {
+            console.log("initializing packet");
+            packet = dataObject;
+          } else {
+            packet = packet + ";" + dataObject;
+            console.log("concatenating");
+          }
           findingsCache.push(
             Finding.fromObject({
               name: "Message Received",
@@ -105,12 +119,15 @@ async function scanGoerliBlocks() {
             })
           );
         }
-      });
+        console.log("packet is " + packet);
+        main(packet);
+      }
+    });
 
-    currentGoerliBlockNumber++;
+    currentTestnetBlockNumber++;
   }
 
-  isScanningGoerli = false;
+  isScanningTestnet = false;
 }
 
 async function handleBlock(blockEvent) {
@@ -123,8 +140,8 @@ async function handleBlock(blockEvent) {
   }
 
   // make sure only one task is running at a time
-  if (!isScanningGoerli) {
-    scanGoerliBlocks();
+  if (!isScanningTestnet) {
+    scanTestnetBlocks();
   }
 
   return findings;
@@ -134,4 +151,3 @@ module.exports = {
   initialize,
   handleBlock,
 };
-
